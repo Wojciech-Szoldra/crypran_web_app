9ccdf3b59f659ef5a66d3ee83b4627c0621b027f
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-04-21 20:52:43 +0200

    SHA256 test

--- tests_1.py
+++ tests_1.py
@@ -1,10 +1,20 @@
 # Odkodowanie payloadu
-import base64, json, string,random
+import base64, json, string, random, hashlib
 
 data = 'eyJwYXNzd29yZCI6IjctaEZrYDk7QjklPiJ9'
 decoded = base64.urlsafe_b64decode(data + "==")
 print(json.loads(decoded))
 
-pass_seq = string.ascii_letters + string.digits + string.punctuation
-test = ''.join(random.choice(pass_seq) for i in range(12))
-print(test)
+def generate_password():
+    pass_seq = string.ascii_letters + string.digits + string.punctuation
+    return''.join(random.choice(pass_seq) for i in range(10))
+
+# SHA256 test
+def hash_test(test_string):
+    return hashlib.sha256(test_string.encode()).hexdigest()
+
+hash1 = hash_test('test1')
+hash2 = hash_test('test1')
+
+if hash1 == hash2:
+    print('Hashes are equal')
