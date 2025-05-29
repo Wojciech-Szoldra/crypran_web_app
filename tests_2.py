96008d5c86c8dc00548c321e87b1d1cd38d60cbe
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-05-29 19:53:22 +0200

    Deleting test files

--- tests_2.py
+++
@@ -1,7 +0,0 @@
-import hmac, hashlib
-
-secret_key = b"moj_tajny_klucz"
-message    = b"Dane do uwierzytelnienia"
-
-h = hmac.new(secret_key, message, hashlib.sha256)
-print("HMAC-SHA256:", h.hexdigest())
